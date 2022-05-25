2913b9248aadf6a5a77d275c874467a50072868c
muhammadihhsan <muhammadihsan20599@gmail.com>
2022-05-25 22:45:14 +0700

    firstcommit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#1e3a8a',
+        second: '#171717',
+        third: '#404040',
+      },
+      screens: {
+        'xl': '1080px',
+      },
+    },
+  },
+  plugins: [],
+}
